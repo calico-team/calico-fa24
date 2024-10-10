db3208ff145b8fb1953cf87d1b65d2c74142d6a0
Lenoplus42 <lenoplus42@berkeley.edu>
2024-10-09 18:18:20 -0700

    Python Template

--- scv/templates/scv_template.py
+++ scv/templates/scv_template.py
@@ -1,4 +1,4 @@
-def solve(R: int, C: int, S: str) -> str:
+def solve(M: int, N: int, G: list) -> str:
     """
     Return the shape of displayed by ASCII string S of dimensions R x C
     
@@ -13,10 +13,13 @@ def solve(R: int, C: int, S: str) -> str:
 def main():
     T = int(input())
     for _ in range(T):
-        temp= input().split()
-        S = input()
-        R, C = int(temp[0]), int(temp[1])
-        print(solve(R, C, S))
+        M, N = map(int, input().split())
+        G = []
+        for _ in range(M):
+            row = list(input().strip())
+            G.append(row)
+        print(G)
+        solve(M, N, G)
 
 if __name__ == '__main__':
     main()
