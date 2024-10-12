04ae9fab36baef7bcaa89aac2507f95404525219
Lenoplus42 <lenoplus42@berkeley.edu>
2024-10-11 19:07:49 -0700

    C++ template

--- scv/templates/scv_template.cpp
+++ scv/templates/scv_template.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 /**
- * Return the shape of displayed by ASCII string S of dimensions R x C
+ * Return the shape of displayed by ASCII string S of dimensions M x N
  *
  * S: a string representing an ASCII picture
- * R: integer for number of rows
- * C: integer for number of columns
+ * M: integer for number of rows
+ * N: integer for number of columns
  */
-String solve(int R, int C, String S) {
+String solve(int M, int N, char G[100][100]) {
     // YOUR CODE HERE
     return -1;
 }
@@ -17,11 +17,15 @@ String solve(int R, int C, String S) {
 int main() {
     int T;
     cin >> T;
-    for (int i = 0; i < T; i++) {
-        int R, C;
-        String S;
-        cin >> R >> C;
-        cin >> S;
-        cout << solve(R, C, S) << '\n';
+    for (int c = 0; c < T; c++) {
+        int M, N;
+        char G[100][100]
+        cin >> M >> N;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                cin >> G[i][j];
+            }
+        }
+        cout << solve(M, N, G) << '\n';
     }
 }
