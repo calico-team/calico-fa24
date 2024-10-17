52ae12cf824d333e9b92006f8f5d25e059a13be5
rohan19a <arohan19@gmail.com>
2024-10-16 17:38:32 -0700

    oreo started

---
+++ oreo/templates/add_template.py
@@ -0,0 +1,18 @@
+def solve(S: string) -> string:
+    """
+    Return the sum of A and B.
+
+    S: a string
+    """
+    # YOUR CODE HERE
+    return 0
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        S = input().split()
+        print(solve(S))
+
+if __name__ == '__main__':
+    main()
