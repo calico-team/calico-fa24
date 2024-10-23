68c2cd3ef6b79702f1736ccb7aa078b28a7b654f
rohan19a <arohan19@gmail.com>
2024-10-23 16:46:36 -0700

    c++ template

---
+++ fortnite/templates/fortnite_template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Return the sum of A and B.
+ *
+ * N: A non-negative integer.
+ * H: A non-negative integer.
+ * D: A non-negative integer.
+ * S: A non-negative integer.
+ * P: A non-negative integer.
+ * T: A non-negative integer.
+ */
+int solve(int N, int H, int D, int S, int P, int T) {
+    // YOUR CODE HERE
+    return -1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int N, H, D, S, P, T;
+        cin >> N >> H >> D >> S >> P >> T;
+        cout << solve(N, H, D, S, P, T) << '\n';
+    }
+}
