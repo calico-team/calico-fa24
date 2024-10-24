bdf7c50927b709763de94a0fc3e3007e47b72947
Jeffrey Tong <jeffrey.w.tong@gmail.com>
2024-10-23 18:05:19 -0700

    Add math sols in cpp and java

---
+++ fortnite/submissions/accepted/fortnite_math.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Solution {
+    /**
+     * Return the number of healing items the player needs to use.
+     *
+     * N: starting health
+     * H: amount of healing
+     * D: distance out of the storm in meters
+     * S: running speed in meters per second
+     * P: storm damage per second
+     * L: length of heal in seconds
+     */
+    static int solve(int N, int H, int D, int S, int P, int L) {
+        return (((D / S) * P) - N) / (H - (P * L)) + 1;
+    }
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(in.readLine());
+        for (int i = 0; i < T; i++) {
+            String[] temp = in.readLine().split(" ");
+            int N = Integer.parseInt(temp[0]),
+                H = Integer.parseInt(temp[1]),
+                D = Integer.parseInt(temp[2]),
+                S = Integer.parseInt(temp[3]),
+                P = Integer.parseInt(temp[4]),
+                L = Integer.parseInt(temp[5]);
+            out.println(solve(N, H, D, S, P, L));
+        }
+        out.flush();
+    }
+}
