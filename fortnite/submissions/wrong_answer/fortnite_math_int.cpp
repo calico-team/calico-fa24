c4ae0b553fc0ca8e6ad65cb6ce280947adb6ed76
Jeffrey Tong <jeffrey.w.tong@gmail.com>
2024-10-24 17:14:57 -0700

    Add WA sol using ints

---
+++ fortnite/submissions/wrong_answer/fortnite_math_int.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Return the number of healing items the player needs to use.
+ *
+ * N: starting health
+ * H: amount of healing
+ * D: distance out of the storm in meters
+ * S: running speed in meters per second
+ * P: storm damage per second
+ * L: length of heal in seconds
+ */
+int solve(int N, int H, int D, int S, int P, int L) {
+    return max((((D / S) * P) - N) / (H - (P * L)) + 1, 0);
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int N, H, D, S, P, L;
+        cin >> N >> H >> D >> S >> P >> L;
+        cout << solve(N, H, D, S, P, L) << '\n';
+    }
+}
