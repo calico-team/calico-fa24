8c63cec11e147cfbe9e328a5c8a1b7d4378d4538
Ryan Zhu <ryanzhu@berkeley.edu>
2024-11-16 13:37:31 -0800

    Rezip

--- fortnite/make_zips.py
+++ fortnite/make_zips.py
@@ -24,7 +24,7 @@ The short name of the problem.
 Names must only use lowercase letters and digits and should be contest-unique.
 Names should be short if possible.
 """
-PROBLEM_NAME = 'fortnite3'
+PROBLEM_NAME = 'fortnite_final'
 
 """
 The time limit in seconds.
